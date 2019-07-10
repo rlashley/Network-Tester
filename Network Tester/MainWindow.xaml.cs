1e6b9af318a8571f89c23771b6f2030753271734
Randy Lashley <rlashley415@gmail.com>
2019-07-09 21:27:09 -0500

    Added more methods and functionality to menu. Next step is to create hashing algorithm for GenHash method.

--- Network Tester/MainWindow.xaml.cs
+++ Network Tester/MainWindow.xaml.cs
@@ -22,22 +22,73 @@ namespace Network_Tester
     /// </summary>
     public partial class MainWindow : Window
     {
+        protected string code="";
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        //Method for button click
+        //Button click event, different methods triggered by selected item in the menu
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (listBox1.SelectedIndex.Equals(0))
             {
-                ipCheck();
+                IpCheck();
             }
+
+            if (listBox1.SelectedIndex.Equals(1))
+            {
+                GenHash(code);
+            }
+
+            if (listBox1.SelectedIndex.Equals(4))
+            {
+                List<int> openPorts = new List<int>();
+                openPorts = PortOpen();
+                foreach(int port in openPorts)
+                {
+                textBlock.Text += port +" is open\n";
+                }
+            }
+
+            if (listBox1.SelectedIndex.Equals(5))
+            {
+                textBlock.Text="Hi Wifey! Just adding placeholders to the program.";
+            }
+        }
+
+        //Button to clear out text box
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text="";
+        }
+
+        //Method that generates a hash using SHA256
+        private string GenHash(string cde){
+            textBlock.Text="Initializing Code Beep boop bop, serializing input to 256bit encrypted hash";
+            //Place body of serializing code
+            string hash = "Initializing";
+            return hash;
+        }
+
+        private List<int> PortOpen(){
+            List<int> listOfPorts = new List<int>();
+            TcpClient tcpClient = new TcpClient();
+            for(int i=0;i<30;i++)
+            {
+                try{
+                tcpClient.Connect("127.0.0.1", i);
+                listOfPorts.Add(i);
+                }
+                catch (Exception){
+                }
+            }
+            return listOfPorts;
         }
 		
         //Method that checks IP address on local machine
-		private void ipCheck(){
+		private void IpCheck(){
 			string response="";
 			textBlock.Text = "Checking IP addresses... ";
             var host = Dns.GetHostEntry(Dns.GetHostName());
