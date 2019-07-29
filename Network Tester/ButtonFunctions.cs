2a9259ca2863c757b9921d53ca956e23ac595a40
Randy Lashley <rlashley415@gmail.com>
2019-07-28 22:33:23 -0500

    Added second class for button functions, cleaned up encryption algorithm, added a theme to interface, cleaned up GUI

---
+++ Network Tester/ButtonFunctions.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Windows.Controls;
+
+namespace Network_Tester
+{
+    class ButtonFunctions
+    {
+
+
+        public ButtonFunctions()
+        {
+            
+        }
+
+        //Method that generates a hash using SHA256
+        public string GenHash(string cde, string Password)
+        {
+
+            //Place body of serializing code
+            RijndaelManaged SymmetricKey = new RijndaelManaged();
+            SymmetricKey.GenerateKey();
+            SymmetricKey.GenerateIV();
+
+            // Encrypt the string to an array of bytes.
+            byte[] encrypted = EncryptStringToBytes(cde, SymmetricKey.Key, SymmetricKey.IV);
+
+            //Return the encrypted data
+            MainWindow.AppWindow.Addtext(cde);
+            return Encoding.Default.GetString(encrypted);
+        }
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        //Method that checks IP address on local machine
+        public void IpCheck()
+        {
+            string response = "";
+            MainWindow.AppWindow.Addtext("Checking IP addresses...");
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    response = ip.ToString();
+                }
+            }
+            MainWindow.AppWindow.Addtext("Your local Host Name is " + host.HostName);
+            MainWindow.AppWindow.Addtext("Your local IP is : " + response);
+        }
+
+        public List<int> ServerConnect()
+        {
+
+            List<int> listOfPorts = new List<int>();
+            int port = 10;
+            TcpListener server = null;
+
+            //Assign localhost ip address to ipa
+            IPAddress ipa = Dns.GetHostAddresses("localhost")[1];
+            try
+            {
+
+                server = new TcpListener(ipa, 10);
+                server.Start();
+                MainWindow.AppWindow.Addtext("Connection to server started...");
+
+                TcpClient client = new TcpClient("localhost", port);
+                listOfPorts.Add(port);
+                client.Close();
+                server.Stop();
+
+            }
+            catch (SocketException e)
+            {
+                MainWindow.AppWindow.Addtext(e.Message);
+            }
+            return listOfPorts;
+        }
+
+    }
+}
--- Network Tester/MainWindow.xaml.cs
+++ Network Tester/MainWindow.xaml.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Security.Cryptography;
-using System.Threading;
 
 namespace Network_Tester
 {
@@ -18,135 +12,70 @@ namespace Network_Tester
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string code="";
+        private string code = "";
+        ButtonFunctions buttonFunctions = new ButtonFunctions();
+        public static MainWindow AppWindow;
 
         public MainWindow()
         {
             InitializeComponent();
             ProgressBar ProgBar = new ProgressBar();
+            AppWindow = this;
+        }
+        public void Addtext(string t)
+        {
+            textBlock.Text += (t+"\n");
         }
 
         //Button click event, different methods triggered by selected item in the menu
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+            //IP Check function
             if (listBox1.SelectedIndex.Equals(0))
             {
-                IpCheck();
+                buttonFunctions.IpCheck();
             }
 
+            //Generate encrypted hash function
             if (listBox1.SelectedIndex.Equals(1))
             {
                 if (!inputBox.Text.Equals(""))
                 {
+                    Addtext("Initializing Code, serializing input to 256bit encrypted hash.");
                     string pass = password.Password;
                     code = inputBox.Text;
-                    textBlock.Text=GenHash(code,pass);
+                    textBlock.Text = buttonFunctions.GenHash(code, pass);
                 }
                 else
-                    textBlock.Text="The text box is empty. Please enter the information you would like to be encrypted.";
+                    Addtext("The text box is empty. Please enter the information you would like to be encrypted.");
             }
 
+            //Server connection function
             if (listBox1.SelectedIndex.Equals(4))
             {
                 List<int> openPorts = new List<int>();
-                openPorts = ServerConnect();
-                foreach(int port in openPorts)
+                openPorts = buttonFunctions.ServerConnect();
+                foreach (int port in openPorts)
                 {
-                    textBlock.Text += "\nConnection has been made on port "+port+"\n";
+                    Addtext("Connection has been made on port " + port);
                 }
             }
 
+            //Daisy button(Place holder for now, future features will go here)
             if (listBox1.SelectedIndex.Equals(5))
             {
-                textBlock.Text="Hi Wifey! Just adding placeholders to the program.";
+                Addtext("Hi Wifey! Just adding placeholders to the program.");
             }
         }
 
         //Button to clear out text box
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text="";
-            inputBox.Text="";
+            textBlock.Text = "";
+            inputBox.Text = "";
+            password.Clear();
         }
-
-        //Method that generates a hash using SHA256
-        private string GenHash(string cde, string Password){
-            textBlock.Text="Initializing Code, serializing input to 256bit encrypted hash.";
-            //Place body of serializing code
-
-            string Salt = "FairyUnicornPrincess";
-            string HashAlgorithm = "SHA1";
-            int PasswordIterations = 2;
-            string InitialVector = "OFRna73m*aze01xY";
-            int KeySize = 256;
-
-            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(InitialVector);
-            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(Salt);
-            byte[] PlainTextBytes = Encoding.UTF8.GetBytes(cde);
-            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(Password, SaltValueBytes, HashAlgorithm, PasswordIterations);
-            byte[] KeyBytes = DerivedPassword.GetBytes(KeySize / 8);
-            RijndaelManaged SymmetricKey = new RijndaelManaged();
-            SymmetricKey.Mode = CipherMode.CBC;
-            byte[] CipherTextBytes = null;
-            ICryptoTransform Encryptor = SymmetricKey.CreateEncryptor(KeyBytes, InitialVectorBytes);
-            
-            MemoryStream MemStream = new MemoryStream();
-                
-            CryptoStream CryptoStream = new CryptoStream(MemStream, Encryptor, CryptoStreamMode.Write);
-                    
-            CryptoStream.Write(PlainTextBytes, 0, PlainTextBytes.Length);
-            CryptoStream.FlushFinalBlock();
-            CipherTextBytes = MemStream.ToArray();
-            MemStream.Close();
-            CryptoStream.Close();            
-            SymmetricKey.Clear();
-
-        return Convert.ToBase64String(CipherTextBytes);
-        }
-
-        private List<int> ServerConnect(){
-
-            List<int> listOfPorts = new List<int>();
-            int port = 10;
-            TcpListener server = null;
-
-            //Assign localhost ip address to ipa
-            IPAddress ipa = Dns.GetHostAddresses("localhost")[1];
-            try
-            {
-
-                server = new TcpListener(ipa, 10);
-                server.Start();
-                textBlock.Text += "\nConnection to server started...\n";
-
-                TcpClient client = new TcpClient("localhost", port);
-                listOfPorts.Add(port);
-                client.Close();
-                server.Stop();
-            
-            }
-            catch (SocketException e) 
-            {
-                MessageBox.Show(e.Message);
-            }
-            return listOfPorts;
-        }
-		
-        //Method that checks IP address on local machine
-		private void IpCheck(){
-			string response="";
-			textBlock.Text = "Checking IP addresses... \n";
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-					response = ip.ToString();
-                }
-            }
-            textBlock.Text+="Your local Host Name is "+host.HostName+"\n";
-			textBlock.Text+="Your local IP is : " +response;
-		}
 
         //Methods for any event on click items in list.
         private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
