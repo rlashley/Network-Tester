d312e9a544623977a0a334d25805b0c27fcd33df
Randall Lashley <rlashley415@gmail.com>
2019-08-06 16:50:58 -0500

    Update ViewModel.cs

--- Network Tester/ViewModel.cs
+++ Network Tester/ViewModel.cs
@@ -1,40 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-
 
 namespace Network_Tester
 {
-
-  public class ViewModel : INotifyPropertyChanged
-  {
-    public event PropertyChangedEventHandler PropertyChanged;
-    private string _Textblock;
-    public string Textblock
-    {
-        get => _Textblock;
-        set => SetProperty(ref _Textblock, value);
-    }
-    public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName);
-  }
-  
-  public abstract class ViewModelBase : INotifyPropertyChanged
-  {
-    public event PropertyChangedEventHandler PropertyChanged;
- 
-    protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = null)
-    {
-        if(!EqualityComparer<T>.Default.Equals(field, newValue))
-        {
-            field = newValue;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            return true;
-        }
-        return false;
-    }
-  }
-      
+  public class ViewModel
+  {		
+      public string TextBlock { get;set;}
+  } 
 }
